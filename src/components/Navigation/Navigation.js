7cd9382a92acbff48baf737eff77e44420f13ec9
Diyar Faraj <48604250+diyarfaraj@users.noreply.github.com>
2019-04-19 16:00:00 +0200

    ändrade färg på en av nav länk

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -13,7 +13,7 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
 	} else {
 		return (
 				<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-					<p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Logga in</p>
+					<p onClick={() => onRouteChange('signin')} className='f3 link dim white underline pa3 pointer'>Logga in</p>
 					<p onClick={() => onRouteChange('Register')} className='f3 link dim black underline pa3 pointer'>Registrera dig</p>
 				</nav>
 
