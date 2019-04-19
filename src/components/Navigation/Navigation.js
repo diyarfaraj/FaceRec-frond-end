3c27a1b1e843436b93edea8618bbde676fbfef2a
Diyar Faraj <48604250+diyarfaraj@users.noreply.github.com>
2019-04-19 16:12:38 +0200

    ändrade den andra loggan

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -14,7 +14,7 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
 		return (
 				<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
 					<p onClick={() => onRouteChange('signin')} className='f3 link dim white underline pa3 pointer'>Logga in</p>
-					<p onClick={() => onRouteChange('Register')} className='f3 link dim black underline pa3 pointer'>Registrera dig</p>
+					<p onClick={() => onRouteChange('Register')} className='f3 link dim white underline pa3 pointer'>Registrera dig</p>
 				</nav>
 
 		);
